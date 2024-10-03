41dfdfd2601997e4b1ba59ef96f1cdc2b3a1d65c
lalli-oni <lalli.oni@gmail.com>
2024-10-03 16:28:37 +0200

    Change API_URL to API_HOST

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,16 @@ interface Response {
   children: Array<Child>;
 }
 
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN
 // TODO (LTJ): Find a fitting home for this
-const API_URL = 'https://app.famly.co/api/daycare/tablet/group'
+const API_HOST = 'https://app.famly.co/api/'
 const GROUP_ID = '86413ecf-01a1-44da-ba73-1aeda212a196'
 const INSTITUTION_ID = 'dc4bd858-9e9c-4df7-9386-0d91e42280eb'
 
 // TODO (LTJ): Find a fitting home for this
 const getChildren = async (accessToken: string): Promise<Response> => {
   // TODO (LTJ): Encode URI components using native API
-  const response = await fetch(`${API_URL}?accessToken=${accessToken}&groupId=${GROUP_ID}&institutionId=${INSTITUTION_ID}`)
+  const response = await fetch(`${API_HOST}daycare/tablet/group?accessToken=${accessToken}&groupId=${GROUP_ID}&institutionId=${INSTITUTION_ID}`)
 
   if (!response.ok) {
     throw new Error(`Error fetching child list. ${response.status}`)
@@ -38,11 +39,25 @@ function App() {
   const [error, setError] = useState<null | string>(null)
 
   useEffect(() => {
-    getChildren(import.meta.env.VITE_ACCESS_TOKEN)
+    getChildren(ACCESS_TOKEN)
       .then((response) => delay(2000).then(() => setData(response?.children)))
       .catch(() => setError(`Error fetching list of children.`))
       .finally(() => setIsLoading(false))
   }, [])
+
+  const updateChildLocation = async (childId: string, direction: 'in' | 'out') => {
+    const currentTime = new Date()
+    const formattedTime = `${currentTime.getHours()}:${currentTime.getMinutes()}`
+    // TODO (LTJ): Encode URI components using native API
+    const response = await fetch(`${API_HOST}v2/children/${childId}/${direction === 'in' ? 'checkins' : 'checkout'}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+        pickupTime: direction === 'in' ? formattedTime : undefined
+      })
+    })
+    if (!response.ok) setError(`Error checking ${direction} child`)
+  }
 
   return (
     <>
