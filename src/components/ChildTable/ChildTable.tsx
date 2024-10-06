be85dd76d067b2718ac1c4a0afb7f7a205814a19
lalli-oni <lalli.oni@gmail.com>
2024-10-06 17:59:22 +0200

    Make a dataview of child list Add pageination controls Add current page index label Control page view with dataView state Reset startIndex when children change

--- src/components/ChildTable/ChildTable.tsx
+++ src/components/ChildTable/ChildTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { type Child } from '../../models/child';
 import './ChildTable.module.css';
 
@@ -12,6 +13,19 @@ export interface ChildTableProps {
 }
 
 function ChildTable(props: ChildTableProps) {
+  const {children, pageSize, onCheck} = props
+  // Start index of the page
+  const [startIndex, setStartIndex] = useState(0)
+  const [dataView, setDataView] = useState(children.slice(0, pageSize))
+
+  useEffect(() => {
+    setStartIndex(0)
+  }, [children])
+
+  useEffect(() => {
+    setDataView(children.slice(startIndex, startIndex + 5))
+  }, [startIndex, children])
+
   return (
     <table>
       <caption>Children in The Kind Kindergarten</caption>
@@ -23,18 +37,43 @@ function ChildTable(props: ChildTableProps) {
         </tr>
       </thead>
       <tbody>
-        {props.children.map((child) => 
+        {dataView.map((child) => 
           <tr key={child.childId}>
             <th scope="row">{child.name.fullName}</th>
             <td>{child.checkedIn ? "Checked in" : "Checked out"}</td>
             <td>
               {child.checkedIn ? 
-                <input type="button" onClick={() => props.onCheck(child.childId, 'in')} value="Check in" /> :
-                <input type="button" onClick={() => props.onCheck(child.childId, 'out')} value="Check out" />}
+                <input type="button" onClick={() => onCheck(child.childId, 'in')} value="Check in" /> :
+                <input type="button" onClick={() => onCheck(child.childId, 'out')} value="Check out" />}
             </td>
           </tr>
         )}
       </tbody>
+      <tfoot>
+        <div>
+          <input
+            type="button"
+            onClick={() => {
+              const temp = startIndex - 5
+              if (temp < 0) { setStartIndex(0) } else { setStartIndex(temp) }
+            }}
+            disabled={startIndex < 1}
+            value="Prev"
+          />
+          <input
+            type="button"
+            onClick={() => {
+              const temp = startIndex + 5
+              if (temp > children.length) { setStartIndex(children.length - pageSize) } else { setStartIndex((startIndex + 5)) }
+            }}
+            disabled={(startIndex + pageSize) > children.length}
+            value="Next"
+          />
+        </div>
+        <div>
+          <span>{startIndex + 1} / {dataView[dataView.length - 1].index + 1}</span>
+        </div>
+      </tfoot>
     </table>
   )
 }
